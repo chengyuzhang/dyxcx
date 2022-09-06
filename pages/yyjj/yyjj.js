e926a456a4a2bc195bb4d1bcdbcd8927f7dbd3f7
chengyuzhang <415912809@qq.com>
2022-09-06 14:22:15 +0800

    save

--- pages/yyjj/yyjj.js
+++ pages/yyjj/yyjj.js
@@ -1,9 +1,15 @@
 // /Users/zhangchengyu/Workstation/DcfyMiniApp/dcfy/pages/yyjj/yyjj.js
+import { areaAbout } from '../../request/api.js'
+
 Page({
   data: {
-	  tabIndex: 0
+    tabIndex: 0,
+    id: 2,
+    intro: '',
+    picture: ''
   },
   onLoad: function (options) {
+		this.getAreaDetail()
 
   },
   changeTab(ev){
@@ -12,5 +18,30 @@ Page({
     this.setData({
       tabIndex: idx
     })
-  }
+
+    if(idx == 0){
+      this.setData({
+        id: 2
+      })
+    }else{
+      this.setData({
+        id: 1
+      })
+    }
+    this.getAreaDetail()
+  },
+  getAreaDetail(){
+    areaAbout.getAreaDetail({
+      id: this.data.id
+    }).then(res => {
+      console.log('getAreaDetail-res', res)
+      
+      this.setData({
+        intro: res.data.intro,
+        picture: res.data.picture
+      })
+    }).catch(err => {
+      console.log('getAreaDetail-err', err)
+    })
+  },
 })
